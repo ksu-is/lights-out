d9662e06416eabb025de712f9c76ba85bf5152aa
Declan-Reuschel-KSU <dreusche@students.kennesaw.edu>
2023-01-26 15:39:22 -0500

    resizing done, error handling improved

--- src/error.py
+++ src/error.py
@@ -10,6 +10,15 @@ handling.
 import traceback
 
 def causeError(error_type_string,error_desc_string):
+    '''
+    This function will print out a formatted message describing an error with a given error type and description,
+    along with the full traceback of the problem. This makes the usual traceback message much more visible in a terminal.
+    This function should be used in combination with try: and except: blocks, where a section of code is included
+    in the try: block, and then this function is called in the except: block. This allows for showing clearly
+    formatted errors with known names and descriptions, where errors may be expected.\n
+    IMPORTANT: This is only meant for making errors easier to understand and find. This does not handle errors
+    on its own, or do anything to resolve them.
+    '''
     tmp_string = '''
     ==========================================================================================\n
     ==========================================================================================\n
@@ -28,4 +37,12 @@ def causeError(error_type_string,error_desc_string):
     print(tmp_string)
 
     exit()
-    
+
+def testError():
+    '''
+    This is a function to be used when testing the implementation of the "try: (code) except: causeError" setup
+    in any given file, class, or function. This is not to be included in any final or production version, and should
+    be removed once testing is complete.\n
+    IMPORTANT: This is for testing purposes only, and should be removed once testing is complete!
+    '''
+    raise Exception("This is a testing error, thrown by the testError function in error.py. You should be expecting to see this, as part of an error.py output message. If you are not, it means that this was left someplace where it shouldn't be, and it should be removed")
--- src/game.py
+++ src/game.py
@@ -164,27 +164,32 @@ createEntity(EntityTypeList.Player,"Player",(16,16,8,8),"player")
 
 def render():
 
-    window.fill(DISPLAY_GREY)
-    screen.fill(DISPLAY_BLACK)
+    try:
 
-    for i in EntityList:
-        if (i.EntityVisible == True and i.EntityImageSource != ""):
-            tmp_rect = i.EntityRect
-            tmp_img = pygame.transform.scale_by(getImageSource(i.EntityImageSource).ImageLoad,DISPLAY_SCALE)
-            tmp_imgrect = getImageSourceRect(i.EntityImageSource)
-            screen.blit(tmp_img,((tmp_rect.centerx-tmp_imgrect.width/2)*DISPLAY_SCALE,(tmp_rect.centery-tmp_imgrect.height/2)*DISPLAY_SCALE,0,0))
+        window.fill(DISPLAY_GREY)
+        screen.fill(DISPLAY_BLACK)
 
-    #always render mouse above all entities
-    tmp_img = pygame.transform.scale_by(getImageSource("mouse").ImageLoad,DISPLAY_SCALE)
-    tmp_imgrect = getImageSourceRect("mouse")
-    tmp_pos = pygame.mouse.get_pos()
-    tmp_mx = (tmp_pos[0])-SCREEN_X
-    tmp_my = (tmp_pos[1])-SCREEN_Y
-    tmp_mx = util.clamp(tmp_mx,0,screen.get_width())
-    tmp_my = util.clamp(tmp_my,0,screen.get_height())
-    screen.blit(tmp_img,(tmp_mx-tmp_imgrect.width/2*DISPLAY_SCALE,tmp_my-tmp_imgrect.height/2*DISPLAY_SCALE,0,0))
+        for i in EntityList:
+            if (i.EntityVisible == True and i.EntityImageSource != ""):
+                tmp_rect = i.EntityRect
+                tmp_img = pygame.transform.scale_by(getImageSource(i.EntityImageSource).ImageLoad,DISPLAY_SCALE)
+                tmp_imgrect = getImageSourceRect(i.EntityImageSource)
+                screen.blit(tmp_img,((tmp_rect.centerx-tmp_imgrect.width/2)*DISPLAY_SCALE,(tmp_rect.centery-tmp_imgrect.height/2)*DISPLAY_SCALE,0,0))
 
-    pygame.display.flip()
+        #always render mouse above all entities
+        tmp_img = pygame.transform.scale_by(getImageSource("mouse").ImageLoad,DISPLAY_SCALE)
+        tmp_imgrect = getImageSourceRect("mouse")
+        tmp_pos = pygame.mouse.get_pos()
+        tmp_mx = (tmp_pos[0])-SCREEN_X
+        tmp_my = (tmp_pos[1])-SCREEN_Y
+        tmp_mx = util.clamp(tmp_mx,0,screen.get_width())
+        tmp_my = util.clamp(tmp_my,0,screen.get_height())
+        screen.blit(tmp_img,(tmp_mx-tmp_imgrect.width/2*DISPLAY_SCALE,tmp_my-tmp_imgrect.height/2*DISPLAY_SCALE,0,0))
+
+        pygame.display.flip()
+
+    except:
+        error.causeError("Rendering Error","There was an error in the render() function")
 
 
 #endregion
@@ -213,27 +218,28 @@ while True:
         if event.type == pygame.QUIT: sys.exit()
 
         if event.type == pygame.WINDOWRESIZED:
-            flag = True
-            tmp_width = 1.6
-            tmp_height = 1.2
-            while flag:
-                if (flag and tmp_width < pygame.display.get_window_size()[0]-1.6): tmp_width += 1.6
-                else: flag = False
-                if (flag and tmp_height <pygame.display.get_window_size()[1]-1.2): tmp_height += 1.2
-                else: flag = False
+            try:
+                flag = True
+                tmp_width = 1.6
+                tmp_height = 1.2
+                while flag:
+                    if (flag and tmp_width < pygame.display.get_window_size()[0]-1.6): tmp_width += 1.6
+                    else: flag = False
+                    if (flag and tmp_height <pygame.display.get_window_size()[1]-1.2): tmp_height += 1.2
+                    else: flag = False
 
-            tmp_x = (pygame.display.get_window_size()[0]-tmp_width)/2
-            tmp_y = (pygame.display.get_window_size()[1]-tmp_height)/2
+                tmp_x = (pygame.display.get_window_size()[0]-tmp_width)/2
+                tmp_y = (pygame.display.get_window_size()[1]-tmp_height)/2
 
-            screen = window.subsurface((tmp_x,tmp_y,tmp_width,tmp_height))
+                screen = window.subsurface((tmp_x,tmp_y,tmp_width,tmp_height))
 
-            DISPLAY_SCALE = tmp_width / SCREEN_WIDTH
+                DISPLAY_SCALE = tmp_width / SCREEN_WIDTH
 
-            SCREEN_X = tmp_x
-            SCREEN_Y = tmp_y
+                SCREEN_X = tmp_x
+                SCREEN_Y = tmp_y
 
-            #pygame.transform.scale(screen,pygame.display.get_window_size())
-            print("x: "+str(tmp_x)+", y: "+str(tmp_y))
+            except:
+                error.causeError("Screen Resizing Error","There was an error while resizing the screen, during a WINDOWRESIZED event")
 
         InputList.append(event)
 
